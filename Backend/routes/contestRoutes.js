0561601d9b84cdef8138b8cc6d23633c289a839a
Jay Fanse <jayf29112003@gmail.com>
2024-07-14 23:21:57 +0530

    uncommented routes

--- Backend/routes/contestRoutes.js
+++ Backend/routes/contestRoutes.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const contestController = require('../controller/contestController');
 
 router.get('/past', contestController.getPastContest);
-// router.get('/current', contestController.getCurrentContest);
-// router.get('/upcoming', contestController.getUpcoming);
-
-// router.post('/registerContest', contestController.registerContest);
+router.get('/current', contestController.getCurrentContest);
+router.get('/upcoming', contestController.getUpcoming);
+router.post('/registerContest', contestController.registerContest);
 
 module.exports = router;
