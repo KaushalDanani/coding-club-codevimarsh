412aef3905ecc9cd3daa7fad95fa90af5672b0a2
Kaushal <kdanani209@gmail.com>
2024-08-12 14:20:20 +0530

    dynamic PORT in env

--- Backend/serverPort.js
+++ Backend/serverPort.js
@@ -1,5 +1,5 @@
 const app = require('./index.js');
-const PORT = process.env.REACT_APP_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
